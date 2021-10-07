2a22c98d77fc46a1a563e5f468f7995584d28bf9
VlasovDmitriy <vlasov.dmitriy.dv1@gmail.com>
2021-10-07 23:21:07 +0300

    ProblemC

--- ProblemB/problem_b.cpp
+++ ProblemB/problem_b.cpp
@@ -1,6 +1,6 @@
-#if 1
+#if 0
 
-#define DEBUG 1
+#define DEBUG 0
 
 #include <iostream>
 #include <fstream>
@@ -19,46 +19,47 @@ istream &input = std::cin;
 ostream &output = std::cout;
 #endif
 
-const int MOD = (int)1e9 + 7;
+typedef long long ll;
+const ll MOD = (ll)1e9 + 7;
 
 int main()
 {
-  while (runs-- > 0)
-  {
-    int P = 29;
+	while (runs-- > 0)
+	{
+		ll P = 29;
 
 
-    string str1, str2;
-    input >> str1 >> str2;
+		string str1, str2;
+		input >> str1 >> str2;
 
-    vector<int> pPow(str2.size());
+		vector<ll> pPow(max(str1.size(), str2.size()));
 
-    pPow[0] = 1;
-    for (int i = 1; i < str1.size(); i++)
-      pPow[i] = pPow[i - 1] * P % MOD;
+		pPow[0] = 1;
+		for (int i = 1; i < max(str1.size(), str2.size()); i++)
+			pPow[i] = pPow[i - 1] * P % MOD;
 
-    int hash2 = 0;
-    for (int i = 0; i < str2.size(); i++)
-    {
-      hash2 += (str2[i] - 'a') * pPow[i];
-      hash2 %= MOD;
-    }
-    
-    vector<int> ans;
-    
-    vector<int> hash1;
-    for (int i = 0; i < str1.size(); i++)
-    {
-      hash1[i + 1] = (str1[i] - 'a') * pPow[i] + (i > 1 ? hash1[i] : 0);
-      hash1[i + 1] %= MOD;
+		ll hash2 = 0;
+		for (int i = 0; i < str2.size(); i++)
+		{
+			hash2 += (str2[i] - 'a') * pPow[i];
+			hash2 %= MOD;
+		}
 
-      if (i >= str2.size() - 1 && hash1[i + 1] - hash1[i + 1 - str2.size()] == hash2 * pPow[i - str2.size() + 1] % MOD)
-        ans.push_back(i - str2.size() + 1);
-    }
-    
-    for (int i = 0; i < ans.size(); i++)
-      output << ans[i] << (i == ans.size() - 1 ? "/n" : " ");
-  }
+		vector<int> ans;
+
+		vector<ll> hash1(str1.size() + 1);
+		for (int i = 0; i < str1.size(); i++)
+		{
+			hash1[i + 1] = (str1[i] - 'a') * pPow[i] + (i > 0 ? hash1[i] : 0);
+			hash1[i + 1] %= MOD;
+
+			if (i >= str2.size() - 1 && (hash1[i + 1] - hash1[i + 1 - str2.size()] + MOD) % MOD == hash2 * pPow[i - str2.size() + 1] % MOD)
+				ans.push_back(i - str2.size() + 1);
+		}
+
+		for (int i = 0; i < ans.size(); i++)
+			output << ans[i] << (i == ans.size() - 1 ? "\n" : " ");
+	}
 }
 
 
--- ProblemC/problem_c.cpp
+++ ProblemC/problem_c.cpp
@@ -5,8 +5,11 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <string>
 
-#if DEBUG 1
+using namespace std;
+
+#if DEBUG
 int runs = 1;
 ifstream input("ProblemC/in.txt");
 ofstream output("ProblemC/out.txt");
@@ -17,29 +20,55 @@ ostream &output = std::cout;
 #endif
 
 
-void (string &str, vector<int> &pF)
+void getPrefixFunc(string &str, vector<int> &pref)
 {
-  for (int i = 1; i < str.size(); i++)
-  {
-    int k = pF[i - 1];
-    while k > 0 && str[k] != str[i]:
-      k = pF[k - 1];
-      
-    if (str[k] == str[i]):
-      k++;
-      
-    pF[i] = k;
-  }
+	pref.resize(str.size());
+	pref[0] = 0;
+	for (int i = 1; i < str.size(); i++)
+	{
+		int k = pref[i - 1];
+
+		while (k > 0 && str[i] != str[k])
+			k = pref[k - 1];
+
+		if (str[i] == str[k])
+			k++;
+
+		pref[i] = k;
+	}
 }
 
 
 int main()
 {
-  while (runs-- > 0)
-  {
-    
-  }
-  
+	while (runs-- > 0)
+	{
+		string str1, str2, str0;
+
+		input >> str1;
+		input >> str2;
+
+		str0 = str1 + "#" + str2;
+
+		vector<int> pref;
+		getPrefixFunc(str0, pref);
+
+		bool cond = true;
+		for (int i = 0; i < str1.size() - pref[pref.size() - 1]; i++)
+			if (str2[i] != str1[pref[pref.size() - 1] + i])
+			{
+				cond = false;
+				break;
+			}
+
+		if (cond)
+			output << str1.size() - pref[pref.size() - 1] << endl;
+		else
+			output << -1 << "\n";
+	}
+
+	return 0;
+
 }
 
 #endif
